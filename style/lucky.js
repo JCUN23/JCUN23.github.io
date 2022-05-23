63b9212151311d1babe25903317ba1dbc6e8b20e
JCUN23 <55635300+JCUN23@users.noreply.github.com>
2022-05-23 00:28:35 -0400

    Delete style directory

--- style/lucky.js
+++
@@ -1,6 +0,0 @@
-window.onload = (function () {
-    num = Math.floor(Math.random() * 4);
-    const pages = ["../templates/about.html", "../templates/wiki.html", "../templates/resume.html", "../templates/portfolio.html"];
-    document.getElementById("lucky").setAttribute("href", pages[num]);
-    return false;
-});
